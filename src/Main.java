1d844661520db61ffe8a4e512de85d6b88478916
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-12-13 15:10:17 +0700

    add all

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        showMenu();
+    }
+
+    private static void showMenu() {
+        System.out.println("----Phần mềm quản lý khách sạn----");
+        System.out.println("1.Danh sách phòng cho thuê");
+        System.out.println("2.Tạo phòng");
+        System.out.println("3.Tìm kiếm thông tin khách hàng");
+        System.out.println("4.Thanh toán");
+    }
+}
